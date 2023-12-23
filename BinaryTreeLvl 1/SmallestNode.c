6e0aa257a8c448dc5626e6cb942d4e9549b88f4e
Joel George <joelgeorge4002@gmail.com>
2023-12-23 11:46:06 +0530

    BinaryTreeLvl 1/SmallestNode.c

--- BinaryTreeLvl 1/SmallestNode.c
+++ BinaryTreeLvl 1/SmallestNode.c
@@ -87,115 +87,21 @@ struct node*succ(struct node* temp)
 	return temp1;
 	}
 }
-void delete(int item)
-{
-	temp=root;
-	flag=0;
-	while(temp!=NULL && flag==0)
-	{
-		if(temp->data==item)
-		{
-			flag=1;
-		}
-		else if(temp->data >item)
-		{
-			temp1=temp;
-			temp=temp->lchild;
-		}
-		else if(temp->data <item)
-		{
-			temp1=temp;
-			temp=temp->rchild;
-		}
-	}
-	if(flag==1)
-	{
-		if(temp->lchild==NULL&&temp->rchild==NULL)
-		{
-			key=1;
-		}
-		else if(temp->lchild!=NULL&&temp->rchild!=NULL) 
-		{
-			key=3;
-		}
-		else
-		{
-			key=2;
-		}
-	}
-	else
-	{
-		printf("Item not found \n");
-	}
-	if(key==1)
-	{
-		if(temp1->lchild==temp)
-		{
-			temp1->lchild=NULL;
-		}
-		else if(temp1->rchild==temp)
-		{
-			temp1->rchild=NULL;
-		}
-	}
-	if(key==2)
-	{
-		if(temp1->rchild==temp)
-		{
-			if(temp->lchild==NULL)
-			{
-				temp1->rchild=temp->rchild;
-			}
-			else
-			{
-				temp1->rchild=temp->lchild;
-			}
-		}
-		if(temp1->lchild==temp)
-		{
-			if(temp->lchild==NULL)
-			{
-				temp1->lchild=temp->rchild;
-			}
-			else
-			{
-				temp1->lchild=temp->lchild;
-			}
-		}
-	}
-
-	if(key==3)
-		{
-			temp2=temp;
-    	 		temp3=succ(temp);
-			item=temp3->data;
-			delete(item);
-			temp2->data=item;
-		}
-	
-	}
-
-
-
 void main()
 {
 	
 	do
 	{
-	printf("\n 1.Insert \n2.Delete \n3.Smallest \n ");
+	printf("\n 1.Insert \n2.Smallest \n ");
 	scanf("%d",&ch);
 	switch(ch)
 	{
-		case 1 :	printf("Enter the value to be inserted to the node \n");
+		case 1 :printf("Enter the value to be inserted to the node \n");
 				scanf("%d",&item);
 			 	buildtree(item);
 				break; 
-		case 2 : printf("Enter the item to delete \n");
-				scanf("%d",&item);
-				delete(item);
-				break;
-		case 3 : smallest();
-				break;
+		case 2 :smallest();
+                break;
 	}	
 	  printf(" \n Do you want to quit click '0' or to continue click '1' ");
    	  scanf("%d",&y);
