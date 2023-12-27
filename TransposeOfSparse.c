4fd83a81ccc1e564f611542bd077971c94bc97b8
Joel George <joelgeorge4002@gmail.com>
2023-12-27 17:34:22 +0530

    TransposeOfSparse.c

---
+++ TransposeOfSparse.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+void main()
+{ 
+    int r,c,i,j,a[10][10],t[3][10],s[3][10],k=1,x=1;
+     printf("enter no of rows:");
+    scanf("%d",&r);
+     printf("enter no of cols:");
+    scanf("%d",&c);
+    printf("enter sparse matrix elements:\n");
+    for(i=0;i<r;i++)
+     {
+         for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+         }
+    }
+    printf("the sparse matrix is:\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+           if(a[i][j]!=0)
+            {
+                s[0][k]=i;
+                s[1][k]=j;
+                s[2][k]=a[i][j];
+                k++;
+            }
+         }
+    }
+    printf("sparse matrix representation:\n");
+     s[0][0]=r;
+    s[1][0]=c;
+     s[2][0]=k-1;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            printf("%d\t",s[i][j]);
+        }
+    printf("\n");
+    }
+    for(i=0;i<c;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            if(s[1][j]==i) 
+            {
+                 t[0][x]=s[1][j];
+                 t[1][x]=s[0][j];
+                 t[2][x]=s[2][j];
+                x++;
+            }
+        }
+    }
+    t[0][0]=s[1][0];
+    t[1][0]=s[0][0];
+    t[2][0]=s[2][0];
+    printf("transose of sparse matrix :\n"); 
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<x;j++)
+        {
+            printf("%d\t",t[i][j]);
+        }
+    printf("\n");
+    }
+}
