b27197ec0d5436f93d27c16b001d3771d96d1bcd
Joel George <joelgeorge4002@gmail.com>
2024-02-04 13:54:47 +0530

    LinkedListReversed.c

---
+++ LinkedListReversed.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node* link;
+};
+struct node *head ,*current,*temp,*prev,*next,*newnode;
+int item;
+void insert()
+{
+    printf("Enter the data \n");
+    scanf("%d",&item);
+    newnode = (struct node* )malloc (sizeof(struct node));    
+    newnode->data=item;
+    if(head==NULL)
+    {
+        head = newnode;
+        newnode->link=NULL;
+    }
+    else
+    {
+        temp=head;
+        while(temp->link!=NULL)
+        {
+            temp=temp->link;
+        }
+        temp->link = newnode;
+        newnode->link=NULL;
+    }
+}
+void diaplay()
+{
+    temp=head;
+    printf("List ==>> ");
+    while(temp!=NULL)
+    {
+        printf(" \t %d",temp->data);
+        temp=temp->link;
+    }
+}
+void reverse()
+{
+    current=head;
+    prev=NULL;
+    while(current!=NULL)
+    {
+        
+        next=current->link;
+        current->link=prev;   
+        prev=current;
+        current=next;
+    }
+    head=prev;
+    temp=head;
+    //to display call display
+}
+void main()
+{
+    int ch;
+    do{
+        printf("\n1.insert \n2.display\n3.reverse\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:insert();
+                    break;
+            case 2:diaplay();
+                    break;
+            case 3:reverse();  
+                    break;
+            default:printf("Invalid \n");
+        }
+    }while(ch!=4);
+}
